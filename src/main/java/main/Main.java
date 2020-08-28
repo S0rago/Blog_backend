d6aff235d2269dfeb72297de7dda0b85f4612cb8
S0rago <linehalt10909@yandex.ru>
2020-08-28 14:27:24 +0300

    Frontend import, project restructure, default page set

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,9 @@
+package main;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"main.controller"})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
